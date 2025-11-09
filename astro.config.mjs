f46212cc6dbaa62ceecf6d54d293748f8644b50d
heve.istvan <heve.istvan@gmail.com>
2025-11-09 09:05:33 +0000

    @rollup/plugin-yaml plugin telepítve

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import yaml from '@rollup/plugin-yaml';
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), yaml()]
   }
 });
