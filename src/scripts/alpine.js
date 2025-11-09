c53b79ff96a7b7a17e19adffb8c605294a9d62d3
heve.istvan <heve.istvan@gmail.com>
2025-11-09 06:44:12 +0000

    létrehozás

---
+++ src/scripts/alpine.js
@@ -0,0 +1,8 @@
+import Alpine from 'alpinejs'
+
+// opcionális: Alpine pluginok, ha később használnád
+// import persist from "@alpinejs/persist"
+// Alpine.plugin(persist)
+
+window.Alpine = Alpine
+Alpine.start()
