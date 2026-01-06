72d44c97b30f662a31a200645b6d5b510e2d37aa
heve.istvan <heve.istvan@gmail.com>
2026-01-06 07:40:22 +0000

    kisebb módosítások a callout-on. md helyett mdx formátum létrehozása, hogy be lehessen ágyazni komponesneket a markdown fájlokba

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import yaml from '@rollup/plugin-yaml';
-
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss(), yaml()]
   }
