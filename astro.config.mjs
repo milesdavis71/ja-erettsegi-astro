807e13b4b020cca6ac9afc6202e8a7fdb3fed0df
Istvan <hevesiistvan71@hotmail.com>
2026-01-09 13:02:39 +0100

    lucide ikon installálása, a jelenlegi buttonokba elhelyezve.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
-import yaml from '@rollup/plugin-yaml';
-import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite'
+import yaml from '@rollup/plugin-yaml'
+import mdx from '@astrojs/mdx'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
   vite: {
-    plugins: [tailwindcss(), yaml()]
-  }
-});
+    plugins: [tailwindcss(), yaml()],
+  },
+})
